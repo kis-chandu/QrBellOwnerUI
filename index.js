7567c552d90bcc529a4dd302c9f709ce0bda37c8
chandrakant.gorai <chandrakant.gorai@tadigital.com>
2022-09-19 15:48:55 +0530

    QrBellOwnerUi

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname,"/assets")));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/index.html"));
+})
+
+app.listen("9002",(req,res)=>{
+    console.log("Server started");
+})
